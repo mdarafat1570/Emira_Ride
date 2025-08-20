53ba59b47140b10353e58276715e3d66186b7e2e
MD Arafat Mia <arafat.softifybd@gmail.com>
2025-08-21 03:05:31 +0600

    nav bar add new

--- lib/presentation/screens/dashboard/dashboard_screen.dart
+++ lib/presentation/screens/dashboard/dashboard_screen.dart
@@ -1,25 +1,70 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ovorideuser/core/utils/my_icons.dart';
 import 'package:ovorideuser/core/utils/my_strings.dart';
-import 'package:ovorideuser/data/controller/account/profile_controller.dart'; // Import ProfileController
+import 'package:ovorideuser/data/controller/account/profile_controller.dart';
 import 'package:ovorideuser/data/controller/menu/my_menu_controller.dart';
 import 'package:ovorideuser/data/controller/pusher/global_pusher_controller.dart';
-import 'package:ovorideuser/data/repo/account/profile_repo.dart'; // Import ProfileRepo
+import 'package:ovorideuser/data/repo/account/profile_repo.dart';
 import 'package:ovorideuser/data/repo/auth/general_setting_repo.dart';
 import 'package:ovorideuser/data/repo/menu_repo/menu_repo.dart';
 import 'package:ovorideuser/presentation/components/annotated_region/annotated_region_widget.dart';
 import 'package:ovorideuser/presentation/components/image/custom_svg_picture.dart';
 import 'package:ovorideuser/presentation/screens/home/home_screen.dart';
 import 'package:ovorideuser/presentation/screens/profile_and_settings/profile_and_settings_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:line_icons/line_icons.dart';
 import 'package:ovorideuser/presentation/screens/web_view/scheduled_web_page_loader.dart';
-import '../../../core/utils/dimensions.dart';
-import '../../../core/utils/my_color.dart';
-import '../../components/will_pop_widget.dart';
-import '../../packages/flutter_floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
-import '../drawer/drawer_screen.dart';
-import '../inter_city/inter_city_screen.dart';
+import 'package:ovorideuser/core/utils/my_color.dart';
+import 'package:ovorideuser/presentation/components/will_pop_widget.dart';
+import 'package:ovorideuser/presentation/screens/drawer/drawer_screen.dart';
+import 'package:ovorideuser/presentation/screens/inter_city/inter_city_screen.dart';
+
+// This is a custom widget created to match the design of the first code snippet's navigation item.
+class NavBarItem extends StatelessWidget {
+  final String imagePath;
+  final String label;
+  final bool isSelected;
+  final VoidCallback press;
+  final int index;
+
+  const NavBarItem({
+    super.key,
+    required this.imagePath,
+    required this.label,
+    required this.isSelected,
+    required this.press,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        onTap: press,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomSvgPicture(
+              image: imagePath,
+              height: 20,
+              width: 20,
+              color: isSelected ? MyColor.primaryColor : MyColor.colorGreyIcon,
+            ),
+            const SizedBox(height: 5),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 10,
+                color: isSelected ? MyColor.primaryColor : MyColor.colorGreyIcon,
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class DashBoardScreen extends StatefulWidget {
   const DashBoardScreen({super.key});
@@ -51,7 +96,7 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
     _widgets = <Widget>[
       HomeScreen(dashBoardScaffoldKey: _dashBoardScaffoldKey),
       InterCityScreen(dashBoardScaffoldKey: _dashBoardScaffoldKey),
-      const ScheduledWebPageLoader(), 
+      const ScheduledWebPageLoader(),
       const ProfileAndSettingsScreen(),
     ];
 
@@ -66,6 +111,7 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
   }
 
   void changeScreen(int val) {
+    Get.find<MyMenuController>().repo.apiClient.storeCurrentTab(val.toString());
     setState(() {
       selectedIndex = val;
     });
@@ -81,68 +127,62 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
           endDrawer: AppDrawerScreen(
             closeFunction: closeDrawer,
             callback: (val) {
-              selectedIndex = val;
-              setState(() {});
+              setState(() {
+                selectedIndex = val;
+              });
               closeDrawer();
             },
           ),
           body: WillPopWidget(child: _widgets[selectedIndex]),
-          bottomNavigationBar: AnnotatedRegionWidget(
-            child: FloatingNavbar(
-              inLine: true,
-              fontSize: Dimensions.fontMedium,
-              backgroundColor: MyColor.colorWhite,
-              unselectedItemColor: MyColor.bodyText,
-              selectedItemColor: MyColor.primaryColor,
-              borderRadius: Dimensions.space50,
-              itemBorderRadius: Dimensions.space50,
-              selectedBackgroundColor: MyColor.primaryColor.withValues(
-                alpha: 0.09,
+          bottomNavigationBar: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            padding: EdgeInsets.zero,
+            child: AnnotatedRegionWidget(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 3),
+                margin: const EdgeInsets.only(bottom: 13, left: 10, right: 10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: MyColor.colorWhite,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [BoxShadow(color: Color.fromARGB(20, 0, 0, 0), offset: Offset(0, 3), blurRadius: 1)],
+                ),
+                height: 55,
+                width: Get.context?.width ?? double.maxFinite,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    NavBarItem(
+                      label: MyStrings.city.tr,
+                      imagePath: MyIcons.cityHome,
+                      index: 0,
+                      isSelected: selectedIndex == 0,
+                      press: () => changeScreen(0),
+                    ),
+                    NavBarItem(
+                      label: MyStrings.interCity_.tr,
+                      imagePath: MyIcons.intercityHome,
+                      index: 1,
+                      isSelected: selectedIndex == 1,
+                      press: () => changeScreen(1),
+                    ),
+                    NavBarItem(
+                      label: MyStrings.preBookSubTitle.tr,
+                      imagePath: MyIcons.hourlyTime,
+                      index: 2,
+                      isSelected: selectedIndex == 2,
+                      press: () => changeScreen(2),
+                    ),
+                    NavBarItem(
+                      label: MyStrings.menu.tr,
+                      imagePath: MyIcons.menu1,
+                      index: 3,
+                      isSelected: selectedIndex == 3,
+                      press: () => changeScreen(3),
+                    ),
+                  ],
+                ),
               ),
-              onTap: (int val) {
-                controller.repo.apiClient.storeCurrentTab(val.toString());
-                changeScreen(val);
-              },
-              margin: const EdgeInsetsDirectional.only(
-                start: Dimensions.space20,
-                end: Dimensions.space20,
-                bottom: Dimensions.space15,
-              ),
-              currentIndex: selectedIndex,
-              items: [
-                FloatingNavbarItem(
-                  icon: LineIcons.home,
-                  title: MyStrings.city.tr,
-                  customWidget: CustomSvgPicture(
-                    image: MyIcons.cityHome,
-                    color: selectedIndex == 0 ? MyColor.primaryColor : MyColor.colorGreyIcon,
-                  ),
-                ),
-                FloatingNavbarItem(
-                  icon: LineIcons.city,
-                  title: MyStrings.interCity_.tr,
-                  customWidget: CustomSvgPicture(
-                    image: MyIcons.intercityHome,
-                    color: selectedIndex == 1 ? MyColor.primaryColor : MyColor.colorGreyIcon,
-                  ),
-                ),
-                FloatingNavbarItem(
-                  icon: LineIcons.calendar, // Changed icon for clarity
-                  title: MyStrings.preBookSubTitle.tr,
-                  customWidget: CustomSvgPicture(
-                    image: MyIcons.hourlyTime, // Changed icon for clarity
-                    color: selectedIndex == 2 ? MyColor.primaryColor : MyColor.colorGreyIcon,
-                  ),
-                ),
-                FloatingNavbarItem(
-                  icon: LineIcons.user, // Changed icon for clarity
-                  title: MyStrings.menu.tr, // This corresponds to the Profile/Settings screen
-                  customWidget: CustomSvgPicture(
-                    image: MyIcons.menu1,
-                    color: selectedIndex == 3 ? MyColor.primaryColor : MyColor.colorGreyIcon,
-                  ),
-                ),
-              ],
             ),
           ),
         );
